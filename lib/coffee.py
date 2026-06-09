bde2d78bc660378090be70af0d4f4a938488e381
Yasin <yasinhassan004@gamil.com>
2026-06-09 19:34:23 +0300

    Implement Coffee size validation and tipping behavior

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,22 +1,18 @@
-#!/usr/bin/env python3
-
 class Coffee:
-    def __init__(self, type, price):
-        self.type = type
-        self.price = price
+    def __init__(self, size):
+        self.size = size
+        self.price = 0
+
     @property
     def size(self):
-        return self.size
+        return self._size
+
     @size.setter
     def size(self, new_size):
-        if type(new_size) == str and new_size in ["Small", "Medium", "Large"]:
-            self.size = new_size
+        if new_size in ["Small", "Medium", "Large"]:
+            self._size = new_size
         else:
-            print("size MUst be Small, Medium, or Large")
-    
+            raise ValueError("Size must be Small, Medium, or Large")
+
     def tip(self, amount):
-        if type(amount) == int and amount > 0:
-            print(f"This coffee is great! Here's a tip of ${amount}.")
-        else:
-            print("Tip amount must be a positive integer")
-    pass
+        self.price += amount
